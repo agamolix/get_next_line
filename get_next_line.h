7d54da76b2335ffa6ca2f5e84ba881cbc1d62860
agamolix <102031132+agamolix@users.noreply.github.com>
2022-04-22 17:22:50 +0200

    Add files via upload

--- get_next_line.h
+++ get_next_line.h
@@ -3,26 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: atrilles <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: atrilles <atrilles@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/31 19:35:18 by atrilles          #+#    #+#             */
-/*   Updated: 2022/02/15 11:52:23 by atrilles         ###   ########.fr       */
+/*   Updated: 2022/04/22 17:21:43 by atrilles         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUFFER_SIZE
-# define BUFFER_SIZE 5
+# define BUFFER_SIZE 10
 #endif
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include <fcntl.h>
 #include <stdlib.h>
 #include <unistd.h>
 
 
-int    ft_strlen(const char *str);
+int    ft_strlen(char *str);
 char	*ft_strjoin(char *s1, char *s2);
 int index_nl(char *s);
 
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: atrilles <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: atrilles <atrilles@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/31 19:35:18 by atrilles          #+#    #+#             */
-/*   Updated: 2022/02/08 16:14:27 by atrilles         ###   ########.fr       */
+/*   Updated: 2022/04/22 17:21:46 by atrilles         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int    ft_strlen(const char *str)
+int    ft_strlen(char *str)
 {
     int i;
 
@@ -27,31 +27,33 @@ int    ft_strlen(const char *str)
 char	*ft_strjoin(char *s1, char *s2)
 {
     char    *res;
-    int i; 
+    int i;
+	int j;
+	const char *s = "";
 
-    if (s1 == 0 && s2 == 0)
-        return (0);
+	if (s1 == 0)
+		s1 = (char *)s;  
     res = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
     if (res == 0)
         return (0);
-    i = 0;
-    while (i < ft_strlen(s1))
-    {
+    i = -1;
+    while (s1[++i])
         res[i] = s1[i];
-        i++;
-    }
-    while(i < ft_strlen(s1) + ft_strlen(s2))
-    {
-        res[i] = s2[i - ft_strlen(s1)];
-        i++; 
-    }
-    res[i] = 0;
+	j = -1;
+    while (s2[++j])
+        res[i + j] = s2[j];
+    res[i + j] = 0;
+	if (s1 != s)
+		free(s1);
     return (res);
 }
 
 int index_nl(char *s)
 {
     int i;
+
+	if (s == 0)
+		return -1;
 
     i = 0;
     while (s[i] && s[i] != '\n')
